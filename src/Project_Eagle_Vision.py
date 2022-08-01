a7934b5e21ab3f57bc9beed7481a1c7409153205
Xianyu Chen <734992689@qq.com>
2022-07-31 22:55:11 -0500

    Add our own spiral drawing DNN with eval mode

--- src/Project_Eagle_Vision.py
+++ src/Project_Eagle_Vision.py
@@ -22,58 +22,8 @@ def load_model(path: str = 'models/checkpoints/checkpoint_best.pth') -> baseline
     model = baseline()
     checkpoint = torch.load(path, map_location=torch.device('cpu'))
     model.load_state_dict(checkpoint["model"], strict=True)
+    model.eval()
     return model
-
-
-@st.cache()
-def load_index_to_label_dict(
-        path: str = 'src/index_to_class_label.json'
-        ) -> dict:
-    """Retrieves and formats the
-    index to class label
-    lookup dictionary needed to
-    make sense of the predictions.
-    When loaded in, the keys are strings, this also
-    processes those keys to integers."""
-    with open(path, 'r') as f:
-        index_to_class_label_dict = json.load(f)
-    index_to_class_label_dict = {
-        int(k): v for k, v in index_to_class_label_dict.items()}
-    return index_to_class_label_dict
-
-
-def load_files_from_s3(
-        keys: list,
-        bucket_name: str = 'bird-classification-bucket'
-        ) -> list:
-    """Retrieves files anonymously from my public S3 bucket"""
-    s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
-    s3_files = []
-    for key in keys:
-        s3_file_raw = s3.get_object(Bucket=bucket_name, Key=key)
-        s3_file_cleaned = s3_file_raw['Body'].read()
-        s3_file_image = Image.open(BytesIO(s3_file_cleaned))
-        s3_files.append(s3_file_image)
-    return s3_files
-
-
-@st.cache()
-def load_s3_file_structure(path: str = 'src/all_image_files.json') -> dict:
-    """Retrieves JSON document outining the S3 file structure"""
-    with open(path, 'r') as f:
-        return json.load(f)
-
-
-@st.cache()
-def load_list_of_images_available(
-        all_image_files: dict,
-        image_files_dtype: str,
-        bird_species: str
-        ) -> list:
-    """Retrieves list of available images given the current selections"""
-    species_dict = all_image_files.get(image_files_dtype)
-    list_of_files = species_dict.get(bird_species)
-    return list_of_files
 
 
 @st.cache()
@@ -101,11 +51,6 @@ def predict(
 
 if __name__ == '__main__':
     model = load_model()
-    index_to_class_label_dict = load_index_to_label_dict()
-    all_image_files = load_s3_file_structure()
-    types_of_birds = sorted(list(all_image_files['test'].keys()))
-    types_of_birds = [bird.title() for bird in types_of_birds]
-
     st.title('Welcome To Tremor Power Quantification For Spiral Drawing!')
     instructions = """
         Upload your own spiral drawing image.
